1633397f2b9e5b4d0f604f90e58f6e2b363a9ae9
gaoey <gaoey@hotmail.com>
2020-03-13 07:11:21 +0700

    Initial Commit

---
+++ IAmRich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IAmRich
+//
+//  Created by gaoey on 13/3/2563 BE.
+//  Copyright © 2563 gaoey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
